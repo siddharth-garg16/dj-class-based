594530b880ff15714759f5b7ff6c83b969d854ca
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-07-30 00:44:21 +0530

    updateview

--- gscbv10/school/models.py
+++ gscbv10/school/models.py
@@ -1,11 +1,7 @@
 from django.db import models
-from django.urls import reverse
 
 # Create your models here.
 class Student(models.Model):
     name = models.CharField(max_length=70)
     email = models.EmailField()
-    password = models.CharField(max_length=70)
-    
-    def get_absolute_url(self):
-        return reverse("thanks")
+    password = models.CharField(max_length=30)
--- gscbv9/school/views.py
+++ gscbv9/school/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.views.generic.base import TemplateView
 from .models import Student
+from django import forms
+
 
 # Create your views here.
 class StudentFormCreateView(CreateView):
@@ -9,6 +11,16 @@ class StudentFormCreateView(CreateView):
     fields = ['name', 'email', 'password']
     #can use template_name here too
     # success_url = "/thanks/"
+
+    #to add css to these fields we need classes--helps us to do that--same as before
+    def get_form(self):
+        form = super().get_form()
+        form.fields['name'].widget = forms.TextInput(attrs = {'class':'myclass'})
+        form.fields['email'].widget = forms.TextInput(attrs = {'class':'myclass'})
+        form.fields['password'].widget = forms.PasswordInput(attrs = {'class':'myclass'})
+        return form
+        #can give placeholders as well etc etc
+        #we can do above things in simpler way if we create a form in forms.py and just import it in views and use it instead of model = student there
 
 class ThanksTemplateView(TemplateView):
     template_name = "school/thanks.html"
