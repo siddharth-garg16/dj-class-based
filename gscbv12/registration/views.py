1975d084514c63e29a6c2bc08e2ef9d4ca00f4e5
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-07-30 17:49:36 +0530

    authentication views with function based views

--- gscbv12/registration/views.py
+++ gscbv12/registration/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+#login required makes sure profile s accessible only when logged in
+@login_required
 def profile(request):
     return render(request, "registration/profile.html")
